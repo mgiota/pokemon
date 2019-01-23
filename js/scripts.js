1becee204e6fa801414bf48c3ac3abf6364afe03
Günther von Häfen <>
2019-01-23 06:34:27 +0100

    1.3 Edited code, created an array with nested objects (Star Wars Characters), created a for loop to put the characters into index.html and highlight Darth Vader

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,30 @@
-alert('Hello world!');
+var repository = [
+  {
+    name: 'Luke Skywalker',
+    height: 172,
+    mass: 77,
+    gender: "male"
+  },
+  {
+    name: 'C-3PO',
+    height: 167,
+    mass: 75,
+    gender: "n/a"
+  },
+  {
+    name: 'Darth Vader',
+    height: 202,
+    mass: 136,
+    gender: "male"
+  }
+];
+
+var highlight = '<span>(I am Lukes father!)</span>'
+
+for (var i = 0; i < repository.length; i++) {
+  if (repository[i].name === 'Darth Vader') {
+    document.write('<div><p>Name: ' + repository[i].name + highlight + '</p><p>Height: ' + repository[i].height + '</p></div>');
+  } else {
+    document.write('<div><p>Name: ' + repository[i].name + '</p><p>Height: ' + repository[i].height + '</p></div>');
+  }
+};
