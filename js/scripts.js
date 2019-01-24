572ff7c21d9d6d9f45b37ea253971f4a9a502d95
Günther von Häfen <>
2019-01-24 06:19:21 +0100

    1.4 Implemented IIFE for the code. Also the work of the for-Loop is now done by forEach() function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,49 @@
-var repository = [
-  {
-    name: 'Luke Skywalker',
-    height: 172,
-    mass: 77,
-    gender: "male"
-  },
-  {
-    name: 'C-3PO',
-    height: 167,
-    mass: 75,
-    gender: "n/a"
-  },
-  {
-    name: 'Darth Vader',
-    height: 202,
-    mass: 136,
-    gender: "male"
-  }
-];
+// New Code using IIFE
 
-var highlight = '<span class="highlight"> I am Lukes father!</span>'
+var starwarsRepository = (function () {
+  var characters = [
+    {
+      name: 'Luke Skywalker',
+      height: 172,
+      mass: 77,
+      gender: "male"
+    },
+    {
+      name: 'C-3PO',
+      height: 167,
+      mass: 75,
+      gender: "n/a"
+    },
+    {
+      name: 'Darth Vader',
+      height: 202,
+      mass: 136,
+      gender: "male"
+    }
+  ];
 
-for (var i = 0; i < repository.length; i++) {
-  if (repository[i].name === 'Darth Vader') {
-    document.write('<div class="content__item"><p>Name: ' + repository[i].name + highlight + '</p><p>Height: ' + repository[i].height + '</p></div>');
+ function add(character) {
+   characters.push(character);
+ }
+
+ function getAll() {
+   return characters;
+ }
+
+ return {
+   add: add,
+   getAll: getAll
+ };
+})();
+
+// forEach() function
+
+starwarsRepository.getAll().forEach(function(character) {
+  var highlight = '<span class="highlight"> I am Lukes father!</span>';
+
+  if (character.name === 'Darth Vader') {
+    document.write('<div class="content__item"><p>Name: ' + character.name + highlight + '</p><p>Height: ' + character.height + '</p></div>');
   } else {
-    document.write('<div class="content__item"><p>Name: ' + repository[i].name + '</p><p>Height: ' + repository[i].height + '</p></div>');
+    document.write('<div class="content__item"><p>Name: ' + character.name + '</p><p>Height: ' + character.height + '</p></div>');
   }
-};
+});
